3ba79d6d255811ac0ae92f364178570dbed9efed
Alex <alexander.stalnacke@gmail.com>
2026-07-02 11:47:03 +0200

    feat: download function

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+path = os.path.expanduser("~/Downloads")
+
+files = (os.listdir(path))
+
+print(f"You have {len(files)} files in Downloads")
+
+f = 0
+total_bytes = sum(
+        os.path.getsize(os.path.join(path,f))
+        for f in files
+        if os.path.isfile(os.path.join(path,f))
+)
+
+total_gb = total_bytes / (1024 * 1024 * 1024)
+rounded_value = (round(total_gb, 2))
+print(f"You have {rounded_value} GB om your downloads")
