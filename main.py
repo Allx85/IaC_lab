9af44a57bf60156787ec977d6ce6c6b03156314e
Jonathan <koffeinmissbrukarn@gmail.com>
2026-07-03 10:35:28 +0200

    add: variables

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import os
 
 path = os.path.expanduser("~/Downloads")
 
+basename= os.path.basename(path)
+
 try:
         files = (os.listdir(path))
 except FileNotFoundError:
-        print("Downloads folder cannot be found.")
+        print(f"{basename} directory cannot be found.")
         exit(1)
        
-print(f"You have {len(files)} files in Downloads folder")
+print(f"You have {len(files)} files in {basename} directory")
 
 total_bytes = sum(
         os.path.getsize(os.path.join(path,f))
@@ -18,4 +20,4 @@ total_bytes = sum(
 
 total_gb = total_bytes / (1024 * 1024 * 1024)
 rounded_value = (round(total_gb, 2))
-print(f"You have {rounded_value} GB in your Downloads folder")
+print(f"You have {rounded_value} GB in your {basename} directory")
